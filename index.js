a97bfe3bd2343aa3974f8803ec094ebaaccdaa0a
bradypp <bradypp44@gmail.com>
2021-01-07 12:34:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const geoip = require('geoip-lite');
 
-http.get({ host: 'api.ipify.org', port: 80, path: '/' }, function (res) {
-  res.on('data', function (ip) {
-    const geo = geoip.lookup(String(ip));
-    console.log(geo);
+http
+  .get('http://api64.ipify.org', res => {
+    res.on('data', ip => {
+      const geo = geoip.lookup(String(ip));
+      console.log(geo);
+    });
+  })
+  .on('error', err => {
+    console.error(err.message);
   });
-});
