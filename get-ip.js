8c90796e3e52fc763ad57d17478f0f06cadf17eb
bradypp <bradypp44@gmail.com>
2021-01-12 14:58:04 +0000

    Update get-ip.js

--- get-ip.js
+++ get-ip.js
@@ -2,7 +2,7 @@ const publicIp = require('public-ip');
 
 (async () => {
   try {
-    const userIp = await publicIp.v4().then;
+    const userIp = await publicIp.v4();
     window.top.postMessage(userIp, '*');
   } catch (error) {
     console.error(error);
