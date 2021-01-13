d1ba07d76bd2ff069bdd3cc9875329c80593744b
bradypp <bradypp44@gmail.com>
2021-01-13 16:48:38 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const app = express();
 app.use('*', cors());
 app.use(express.json({ limit: '10kb' }));
 
-app.get('/:ip', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const { country } = geoip.lookup(req.params.ip);
-    res.status(200).json({ country });
+    console.log(req.socket.remoteAddress);
+    // console.log(req.connection.remoteAddress);
+    res.send('<h1>Hello World!</h1>');
   } catch (error) {
     console.error(error);
   }
 });
+
+// app.get('/:ip', async (req, res) => {
+//   try {
+//     const { country } = geoip.lookup(req.params.ip);
+//     res.status(200).json({ country });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
